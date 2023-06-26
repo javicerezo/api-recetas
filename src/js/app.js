dab804ff8c49bd40613f86402bcee52e77eb91c0
Javi Cerezo <jc.webmob@gmail.com>
2023-06-26 11:27:10 +0200

    frontend terminado

--- src/js/app.js
+++ src/js/app.js
@@ -1,53 +1,55 @@
-// VARIABLES
 
-const resultadosContenedor = document.querySelector('.js-resultados__contenedor');
-const modalContenedor = document.querySelector('.js-modal');
-const contenedorMensaje = document.querySelector('.js-mensaje__contenedor');
-
-const selectCategorias = document.querySelector('#categorias');
-const subnav = document.querySelector('.js-nav__subnav');
-
-
-//EVENTOS
-
-// evento para iniciar la app
-document.addEventListener('DOMContentLoaded', () => {
-    iniciarApp();
-});
-
-//evento para rellenar el select desde la peticion a la API
-selectCategorias.addEventListener('change', seleccionarCategoria);
-
-// evento para mostrar cada receta
-resultadosContenedor.addEventListener('click', (e) => {
-    if(e.target.classList.contains('js-boton')){
-        const id = e.target.getAttribute('id');
-        seleccionarReceta(id, modalContenedor);    
-    }
-})
-
-// evento para el modal de cada receta
-modalContenedor.addEventListener('click', (e) => {
-    if(e.target.classList.contains('fa-xmark') || e.target.classList.contains('c-modal__screen')){
-        limpiarHTML(modalContenedor);
-    }
-    //boton de favoritos
-    if(e.target.classList.contains('js-boton')) {
-        const receta = {
-            id : document.querySelector('.c-modal__boton').getAttribute('data-id'),
-            titulo : document.querySelector('.c-modal__h2').textContent,
-            imagen : document.querySelector('.js-modal__img').getAttribute('src')
+    // VARIABLES
+    
+    const resultadosContenedor = document.querySelector('.js-resultados__contenedor');
+    const modalContenedor = document.querySelector('.js-modal');
+    const contenedorMensaje = document.querySelector('.js-mensaje__contenedor');
+    
+    const selectCategorias = document.querySelector('#categorias');
+    
+    
+    //EVENTOS
+    // evento para iniciar la app
+    document.addEventListener('DOMContentLoaded', () => {
+        iniciarApp();
+    });
+    
+    //evento para rellenar el select desde la peticion a la API
+    selectCategorias.addEventListener('change', e => {
+        seleccionarCategoria(e);
+    });
+    
+    // evento para mostrar cada receta
+    resultadosContenedor.addEventListener('click', (e) => {
+        if(e.target.classList.contains('js-boton')){
+            const id = e.target.getAttribute('id');
+            seleccionarReceta(id, modalContenedor);    
         }
-        
-        const existe = existeFavoritos(receta.id);
-        if (!existe) {
-            agregarFavoritos(receta);
-            imprimirAlerta(contenedorMensaje, 'exito', 'Agregado correctamente');
-            e.target.textContent = 'Eliminar Favoritos';
-        } else {
-            eliminarFavoritos(receta.id);
-            imprimirAlerta(contenedorMensaje, 'error', 'Eliminado correctamente');
-            e.target.textContent = 'Agregar Favoritos';
+    })
+    
+    // evento para el modal de cada receta
+    modalContenedor.addEventListener('click', (e) => {
+        if(e.target.classList.contains('fa-xmark') || e.target.classList.contains('c-modal__screen')){
+            limpiarHTML(modalContenedor);
         }
-    }
-});
+        //boton de favoritos
+        if(e.target.classList.contains('js-boton')) {
+            const receta = {
+                id : document.querySelector('.c-modal__boton').getAttribute('data-id'),
+                titulo : document.querySelector('.c-modal__h2').textContent,
+                imagen : document.querySelector('.js-modal__img').getAttribute('src')
+            }
+            
+            const existe = existeFavoritos(receta.id);
+            if (!existe) {
+                agregarFavoritos(receta);
+                imprimirAlerta(contenedorMensaje, 'exito', 'Agregado correctamente');
+                e.target.textContent = 'Eliminar Favoritos';
+            } else {
+                eliminarFavoritos(receta.id);
+                imprimirAlerta(contenedorMensaje, 'error', 'Eliminado correctamente');
+                e.target.textContent = 'Agregar Favoritos';
+            }
+        }
+    });
+
--- src/js/favoritos.js
+++ src/js/favoritos.js
@@ -1,49 +1,45 @@
 
-const favoritosContenedor = document.querySelector('.js-favoritos__contenedor');
-const modalContenedorFavoritos = document.querySelector('.js-modal');
-const contenedorMensajeFavoritos = document.querySelector('.js-mensaje__contenedor');
-const listadoFavoritos = JSON.parse(localStorage.getItem('listadoFavoritos')) || [];
-
-document.addEventListener('DOMContentLoaded', () => {
-    try {
-        const listadoFavoritos = JSON.parse(localStorage.getItem('listadoFavoritos')) || [];
+    const favoritosContenedor = document.querySelector('.js-favoritos__contenedor');
+    const modalContenedorFavoritos = document.querySelector('.js-modal');
+    const contenedorMensajeFavoritos = document.querySelector('.js-mensaje__contenedor');
+    let listadoFavoritos = [];
+    
+    document.addEventListener('DOMContentLoaded', () => {
+        listadoFavoritos = JSON.parse(localStorage.getItem('listadoFavoritos')) || [];
         cargarFavoritos(listadoFavoritos);
-    } catch (error) {
-        console.log(error);
-    }
-})
-
-favoritosContenedor.addEventListener('click', (e) => {
-    if(e.target.classList.contains('js-boton')){
-        const id = e.target.getAttribute('id');
-        seleccionarReceta(id, modalContenedorFavoritos);    
-    }
-});
-
-modalContenedorFavoritos.addEventListener('click', (e) => {
-    if(e.target.classList.contains('fa-xmark') || e.target.classList.contains('c-modal__screen')){
-        limpiarHTML(modalContenedorFavoritos);
-        limpiarHTML(favoritosContenedor);
-        const listadoFavoritos = JSON.parse(localStorage.getItem('listadoFavoritos')) || [];
-        cargarFavoritos(listadoFavoritos);
-    }
-    //boton de favoritos
-    if(e.target.classList.contains('js-boton')) {
-        const receta = {
-            id : document.querySelector('.c-modal__boton').getAttribute('data-id'),
-            titulo : document.querySelector('.c-modal__h2').textContent,
-            imagen : document.querySelector('.js-modal__img').getAttribute('src')
+    })
+    
+    favoritosContenedor.addEventListener('click', (e) => {
+        if(e.target.classList.contains('js-boton')){
+            const id = e.target.getAttribute('id');
+            seleccionarReceta(id, modalContenedorFavoritos);    
         }
-        
-        const existe = existeFavoritos(receta.id);
-        if (!existe) {
-            agregarFavoritos(receta);
-            imprimirAlerta(contenedorMensajeFavoritos, 'exito', 'Has agregado la receta a favoritos');
-            e.target.textContent = 'Eliminar Favoritos';
-        } else {
-            eliminarFavoritos(receta.id);
-            imprimirAlerta(contenedorMensajeFavoritos, 'error', 'Has eliminado la receta de favoritos');
-            e.target.textContent = 'Agregar Favoritos';
+    });
+    
+    modalContenedorFavoritos.addEventListener('click', (e) => {
+        if(e.target.classList.contains('fa-xmark') || e.target.classList.contains('c-modal__screen')){
+            limpiarHTML(modalContenedorFavoritos);
+            limpiarHTML(favoritosContenedor);
+            const listadoFavoritos = JSON.parse(localStorage.getItem('listadoFavoritos')) || [];
+            cargarFavoritos(listadoFavoritos);
         }
-    }
-});
+        //boton de favoritos
+        if(e.target.classList.contains('js-boton')) {
+            const receta = {
+                id : document.querySelector('.c-modal__boton').getAttribute('data-id'),
+                titulo : document.querySelector('.c-modal__h2').textContent,
+                imagen : document.querySelector('.js-modal__img').getAttribute('src')
+            }
+            
+            const existe = existeFavoritos(receta.id);
+            if (!existe) {
+                agregarFavoritos(receta);
+                imprimirAlerta(contenedorMensajeFavoritos, 'exito', 'Has agregado la receta a favoritos');
+                e.target.textContent = 'Eliminar Favoritos';
+            } else {
+                eliminarFavoritos(receta.id);
+                imprimirAlerta(contenedorMensajeFavoritos, 'error', 'Has eliminado la receta de favoritos');
+                e.target.textContent = 'Agregar Favoritos';
+            }
+        }
+    });
